58d34ae7e5bc271f04753091f6075987b6feec59
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-07-29 19:23:07 +0100

    feat: calculate yearly salary from weekly salary

---
+++ src/FinancePlanner.Core/WageCalculators/Handlers/FromWeeklySalaryHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using FinancePlanner.Core.Shared.Common.Interfaces;
+using FinancePlanner.Core.Shared.Common.Models;
+
+namespace FinancePlanner.Core.WageCalculator.Handlers;
+
+public class FromWeeklySalaryHandler : IHandler
+{
+    public HandlerResult Handle(decimal salary)
+    {
+        var weeksInCurrentYear = ISOWeek.GetWeeksInYear(System.DateTime.Now.Year);
+
+        return new HandlerResult
+        {
+            YearlySalary = salary * weeksInCurrentYear,
+        };
+    }
+}
---
+++ src/FinancePlanner.Tests/GivenASalary/WhenCalculatingFromAWeeklyIncome.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Enumeration;
+using FinancePlanner.Core.Shared.Common.Models;
+using FinancePlanner.Core.WageCalculator.Handlers;
+
+namespace FinancePlanner.Tests.GivenYearlySalary;
+
+// Injects into setup instead of method for class wide test
+[TestFixture(500, 26000)]           // this doesn't account for leap years properly - see next commit
+public class WhenCalculatingFromAWeeklyIncome
+{
+    private HandlerResult _actualSalary;
+    private readonly decimal _salary;
+    private readonly decimal _expectedSalary;
+
+    public WhenCalculatingFromAWeeklyIncome(double salary, double expectedSalary)
+    {
+        _salary = (decimal)salary;
+        _expectedSalary = (decimal)expectedSalary;
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        var sut = new FromWeeklySalaryHandler();
+        _actualSalary = sut.Handle(_salary);
+    }
+    
+    [Test]
+    public void ThenTheCorrectYearlyIncomeIsCalculated()
+    {
+        Assert.That(_actualSalary.YearlySalary, Is.EqualTo(_expectedSalary));
+    }
+}
