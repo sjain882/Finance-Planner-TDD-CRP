2b0914c19c7af1de2bbc76b36e4296ec473fcc31
sjain882 <43217178+sjain882@users.noreply.github.com>
2025-08-03 16:09:47 +0100

    test: additional rate tax band

---
+++ src/FinancePlanner.Tests/GivenATaxCodeCalculation/WhenTaxCodeIsL/WhenSalaryIsWithinAdditionalRateTaxBand.cs
@@ -0,0 +1,34 @@
+﻿using FinancePlanner.Core.Shared.Common.Interfaces;
+using FinancePlanner.Core.WageCalculators;
+using FinancePlanner.Core.WageCalculators.Handlers.TaxCode;
+using Moq;
+
+namespace FinancePlanner.Tests.GivenATaxCodeCalculation.WhenTaxCodeIsL;
+
+public class WhenSalaryIsWithinAdditionalRateAllowanceBand
+{
+    private HandlerResult _result;
+
+    [SetUp]
+    public void Setup()
+    {
+        var grossYearlyWageMock = new Mock<IHandler>();
+        grossYearlyWageMock
+            .Setup(x => x.Handle(It.IsAny<decimal>()))
+            .Returns(new HandlerResult
+            {
+                YearlySalary = 130000m,
+            });
+        
+        // Tax code of 1257L = 12570 of personal allowance
+        var x = new CalculateTaxCodeLHandler(grossYearlyWageMock.Object, 12570m);
+
+        _result = x.Handle(130000);
+    }
+
+    [Test]
+    public void ThenCorrectTaxedAmountIsCalculated()
+    {
+        Assert.That(_result.TaxedAmount, Is.EqualTo(39675));
+    }
+}
